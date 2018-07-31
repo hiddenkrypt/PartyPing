6bdd2214491ccc50ed03f0d1af67659f3240dfb4
Rav-T <hiddenkrypt@gmail.com>
2018-07-30 20:46:37 -0700

    capitalized constants, moved game join logic to game module, along with player list

--- server.js
+++ server.js
@@ -1,42 +1,20 @@
 //node server start
 
-const express = require( 'express' );
-const app = express();
-const http = require ( 'http' ).Server( app );
-const io = require( 'socket.io' )( http, { origins: '*:*'} );
+const EXPRESS = require( 'express' );
+const APP = EXPRESS();
+const HTTP = require ( 'http' ).Server( APP );
+const IO = require( 'socket.io' )( HTTP, { origins: '*:*'} );
 
-var players = [];
-const game = require( "partyping.js" )(players);
-app.use( express.static( "pub" ) );
+const GAME = require( "./partyping.js" )();
+APP.use( EXPRESS.static( "pub" ) );
 
-http.listen( 80, function() {
+HTTP.listen( 8082, function() {
   console.log( "Server is listening on port 80." );
 });
 
-io.on ("connect", function( socket ) {
+IO.on ("connect", function( socket ) {
   console.log( "Client connected");
   socket.on("attemptJoin", function(name){
-    if(!players.filter((player)=>{
-      return player.name === name;
-    })){
-      socket.emit("rejected", {reason: "Another player is using that name!"});
-    }
-    else if(players.size === PLAYER_MAX){
-      socket.emit("rejected", {reason: "Server is full!"});
-    } else{
-      addNewPlayer(socket, name);
-    }
+    GAME.attemptJoin(socket, name);
   });
 });
-
-function addNewPlayer(socket, name){
-  var newPlayer = {};
-  newPlayer.name = name;
-  newPlayer.socket = socket;
-  players.push(newPlayer);
-  socket.on( "disconnect", function() {
-    console.log( "Client disconnected" );
-    newPlayer.socket = null;
-    players = players.filter((player)=> player.name !== newPlayer.name);
-  });
-}
