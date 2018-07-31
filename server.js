e1460067dcd016985a509dbdb680d51fd0c8a121
Rav-T <hiddenkrypt@gmail.com>
2018-07-30 20:57:07 -0700

    added httpshutdown module for clean shutdowns, and a debug timeout to retire the server cleanly

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const EXPRESS = require( 'express' );
 const APP = EXPRESS();
 const HTTP = require ( 'http' ).Server( APP );
 const IO = require( 'socket.io' )( HTTP, { origins: '*:*'} );
-
+const SERVER = require('http-shutdown')(HTTP);
 const GAME = require( "./partyping.js" )();
 APP.use( EXPRESS.static( "pub" ) );
 
-HTTP.listen( 8082, function() {
-  console.log( "Server is listening on port 80." );
+
+SERVER.listen( 8082, function() {
+  console.log( "Server is listening on port 8082." );
 });
 
 IO.on ("connect", function( socket ) {
@@ -18,3 +19,9 @@ IO.on ("connect", function( socket ) {
     GAME.attemptJoin(socket, name);
   });
 });
+
+setTimeout(()=>{
+  SERVER.shutdown(()=>{
+    console.log("All cleaned up");
+  });
+}, 60000);
