c5fb07d68755a2d78866eb43b72ffc0f49271624
Rav-T <hiddenkrypt@gmail.com>
2018-07-30 20:02:29 -0700

    added rudimentary player handling

--- server.js
+++ server.js
@@ -5,16 +5,36 @@ const app = express();
 const http = require ( 'http' ).Server( app );
 const io = require( 'socket.io' )( http, { origins: '*:*'} );
 
-io.on ("connect", function( socket ) {
-  console.log( "Client connected");
-  socket.on( "disconnect", function() {
-    console.log( "Client disconnected" );
-  });
-});
-
-
 app.use( express.static( "pub" ) );
 
 http.listen( 80, function() {
   console.log( "Server is listening on port 80." );
 });
+
+io.on ("connect", function( socket ) {
+  console.log( "Client connected");
+  socket.on("attemptJoin", function(name){
+    if(!players.filter((player)=>{
+      return player.name === name;
+    })){
+      socket.emit("rejected", {reason: "Another player is using that name!"});
+    }
+    else if(players.size === PLAYER_MAX){
+      socket.emit("rejected", {reason: "Server is full!"});
+    } else{
+      addNewPlayer(socket, name);
+    }
+  });
+});
+
+function addNewPlayer(socket, name){
+  var newPlayer = {};
+  newPlayer.name = name;
+  newPlayer.socket = socket;
+  players.push(newPlayer);
+  socket.on( "disconnect", function() {
+    console.log( "Client disconnected" );
+    newPlayer.socket = null;
+    players = players.filter((player)=> player.name !== newPlayer.name);
+  });
+}
